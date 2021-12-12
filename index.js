d5a757e4db9c1d7aba87d56397a2439ac54b0a11
Jessica Sies <jessica.sies@gmail.com>
2021-12-12 14:36:47 +0100

    Small improvements and refactoring

--- index.js
+++ index.js
@@ -7,26 +7,20 @@
     document.body.dataset.page = link
   }
 
-  document.addEventListener('DOMContentLoaded', setScrollbarOffset)
-
-  function setScrollbarOffset() {
-    document.documentElement.style.setProperty('--scrollbar-offset', `${innerWidth - document.documentElement.clientWidth}px`)
-  }
-
-  window.addEventListener('resize', () => {
-    if (!document.documentElement.classList.contains('fixed')) {
-      setScrollbarOffset()
-    }
-  })
+  new ResizeObserver((entries) => {
+    entries[0].target.style.setProperty('--scrollbar-offset', `${innerWidth - entries[0].target.clientWidth}px`)
+  }).observe(document.documentElement)
 
   document.getElementById('intro').addEventListener('click', (event) => {
     if (event.currentTarget.paused) {
-      event.currentTarget.className = 'active'
       event.currentTarget.play()
     } else {
       event.currentTarget.pause()
-      event.currentTarget.classList.remove('active')
     }
+  })
+
+  document.getElementById('intro').addEventListener('play', (event) => {
+    event.currentTarget.className = 'active'
   })
 
   document.getElementById('intro').addEventListener('pause', (event) => {
@@ -58,115 +52,35 @@
     event.currentTarget.classList.toggle('active')
 
     document.getElementById('tiles').classList.toggle(event.currentTarget.textContent)
-
-    setScrollbarOffset()
   }
 
   const content = [
     {
-      media: [
-        {
-          type: 'video',
-          file: 'PulitzerFB_v3.mp4'
-        }
-      ],
+      media: ['PulitzerFB_v3.mp4'],
       text: 'In this exciting project for Pulitzer Amsterdam, commissioned by Orange Hotel Marketing, I got to make a Facebook advertising video for their fall promotion. I animated the outline of the Amsterdam canal houses using Adobe After Effects. Combining this animation with pictures, text and the falling leaves in Premiere Pro, made for a successful result that taught me a lot. The promotion videos were released across Facebook and Instagram in 5 different languages.'
     }, {
-      media: [
-        {
-          type: 'video',
-          file: 'Campinav3.mp4'
-        }, {
-          type: 'video',
-          file: 'MilieuV3.mp4'
-        }, {
-          type: 'video',
-          file: '1 Uitgemolken Den Haag v2.mp4'
-        }, {
-          type: 'image',
-          file: '2 Biteback.png'
-        }
-      ],
+      media: ['Campinav3.mp4', 'MilieuV3.mp4', '1 Uitgemolken Den Haag v2.mp4', '2 Biteback.png'],
       text: 'As a voluntary member of animal rights organization Bite Back, I have helped edit videos and design social media posts. I have created videos to support the campaign ‘Uitgemolken’ which advocates for cows that are being used for their milk. I have also made images for Instagram that combine text and compelling images found online.'
     }, {
-      media: [
-        {
-          type: 'video',
-          file: 'Jizen.mp4'
-        }, {
-          type: 'video',
-          file: 'Sebastien_FINAL_Kort.mp4'
-        }, {
-          type: 'image',
-          file: 'Potjes.jpg'
-        }
-      ],
+      media: ['Jizen.mp4', 'Sebastien_FINAL_Kort.mp4', 'Potjes.jpg'],
       text: 'Jizen Responsible Marketing was founded to challenge conventional hiring practices based on experience only. Rather, a company should look at the person and their complete set of qualities, personality etc. In the first ‘season’ of this project, I was recruited as a video editor, contributing to the campaign for a new product as well as helping other participants with their personal branding. So far, I have made promotional videos, interviews and designs for characters of the product.'
     }, {
-      media: [
-        {
-          type: 'image',
-          file: '1 Under your Wing poster.jpg'
-        }, {
-          type: 'video',
-          file: '2 game trailer under your wing.mp4'
-        }, {
-          type: 'image',
-          file: '3 Controls.jpg'
-        }, {
-          type: 'image',
-          file: '4 Map1.5.jpg'
-        }
-      ],
+      media: ['1 Under your Wing poster.jpg', '2 game trailer under your wing.mp4', '3 Controls.jpg', '4 Map1.5.jpg'],
       text: 'This game was made for the Global Game Jam, an event that brings together game developers, artists and musicians to make a game in under 48 hours. Together with four other team members, I made the 2D puzzle/platformer ‘Under Your Wing’. In this game, a miner is trapped in a coal mine together with his canary bird. The objective is to escape together, solving puzzles along the way. In this project I was responsible for a multitude of jobs like: game concept, puzzle design, social media (Twitter), game poster and game trailer. This was a very interesting project which taught me more about making games.'
     }, {
-      media: [
-        {
-          type: 'video',
-          file: 'Vmarkt opening HOOG.mp4'
-        }
-      ],
+      media: ['Vmarkt opening HOOG.mp4'],
       text: 'Vmarkt is a vegan supermarket that opened their second store in Utrecht in 2021. As a vegan myself (since 2017), I offered to make a video during their first day. In this short video, made for social media, people can get a first look inside the store.'
     }, {
-      media: [
-        {
-          type: 'video',
-          file: 'Symposium_ Sterk in Sport, ben jij erbij_.mp4'
-        }
-      ],
+      media: ['Symposium_ Sterk in Sport, ben jij erbij_.mp4'],
       text: 'In this full time job as Content & Social Media Marketeer, I was responsible for the daily content on social media, as well as communications within the company. A very diverse job that taught me a lot about marketing and social media. One of my favorite assignments was to create a short video, inviting my colleagues to a symposium about sports physiotherapy. I decided to combine motion graphics with footage of colleagues themselves to create a dynamic video. The complete campaign resulted in the most confirmed attendees to a symposium yet!'
     }, {
-      media: [
-        {
-          type: 'video',
-          file: '1 Rabbit Run.mp4'
-        }, {
-          type: 'image',
-          file: '2 Rabbit gif.gif'
-        }
-      ],
+      media: ['1 Rabbit Run.mp4', '2 Rabbit gif.gif'],
       text: 'Rabbit Run is a 2D platformer in which you are a rabbit, racing to collect all carrots as quickly as possible. Once you return to your home, you can try again, racing your fastest time. This was my very first game project, made together with programmer Julian Vos. In this project I was responsible for all game art, music and sound effects. It was especially interesting to learn how to animate a running rabbit.'
     }, {
-      media: [
-        {
-          type: 'video',
-          file: '1 Map_oceania.mp4'
-        }, {
-          type: 'video',
-          file: '2 Oceania_clip.mp4'
-        }
-      ],
+      media: ['1 Map_oceania.mp4', '2 Oceania_clip.mp4'],
       text: 'In 2017/2018 I traveled across Australia, New Zealand, Fiji and Taiwan. When I came back, I made a film using the footage that I took. This project allowed me to practice animation, for which I used Motion. To capture the feel of the different legs of the journey, I used music by local artists and newsclips to tell the story.'
     }, {
-      media: [
-        {
-          type: 'video',
-          file: 'Pip3.mp4'
-        }, {
-          type: 'video',
-          file: 'Pip2.mp4'
-        }
-      ],
+      media: ['Pip3.mp4', 'Pip2.mp4'],
       text: 'Arita Baaijens is a modern day explorer, who lived in the Sahara desert for 20 years before returning to the Netherlands to explore her home country. During the project ‘Paradijs in de Polder’ I got to support this inspiring individual, making videos to accompany her newest book. My main objective during this project was to mirror Arita’s outlook on nature and the way in which humans interact with nature. This meant choosing a rather minimal way of video editing, showing nature and people in a calm way and letting nature ‘speak for itself’.'
     }
   ]
@@ -209,9 +123,7 @@
 
     const media = []
     for (let i = 0; i < content[tile].media.length; i++) {
-      if (content[tile].media[i].type === 'image') {
-        media.push(document.createElement('img'))
-      } else {
+      if (content[tile].media[i].endsWith('.mp4')) {
         media.push(document.createElement('video'))
 
         media[i].preload = 'auto'
@@ -221,17 +133,19 @@
         media[i].addEventListener('contextmenu', (event) => {
           event.preventDefault()
         })
+      } else {
+        media.push(document.createElement('img'))
       }
 
       document.getElementById('content').insertBefore(media[i], document.getElementById('content').lastElementChild)
     }
     for (let i = 0; i < content[tile].media.length - 1; i++) {
-      media[i][content[tile].media[i].type === 'image' ? 'onload' : 'oncanplaythrough'] = () => {
-        media[i + 1].src = `media/${content[tile].media[i + 1].file}`
+      media[i][content[tile].media[i].endsWith('.mp4') ? 'oncanplaythrough' : 'onload'] = () => {
+        media[i + 1].src = `media/${content[tile].media[i + 1]}`
       }
     }
 
-    media[0].src = `media/${content[tile].media[0].file}`
+    media[0].src = `media/${content[tile].media[0]}`
     document.getElementById('content').lastElementChild.textContent = content[tile].text
     document.getElementById('content').scrollTop = 0
 
@@ -286,8 +200,6 @@
     }
 
     document.documentElement.classList.remove('fixed')
-
-    setScrollbarOffset()
   }
 
   document.querySelectorAll('nav button').forEach((button) => {
@@ -306,9 +218,7 @@
     document.body.classList.add('fadeout')
 
     document.body.addEventListener('animationend', () => {
-      if (document.documentElement.classList.contains('fixed')) {
-        close(document.getElementById('modal'))
-      } else if (!document.getElementById('intro').paused) {
+      if (document.body.dataset.page === 'home') {
         document.getElementById('intro').pause()
       }
 
@@ -316,8 +226,6 @@
       document.body.dataset.page = link
 
       document.documentElement.scrollTop = 0
-
-      setScrollbarOffset()
     }, { once: true })
 
     document.getElementsByTagName('aside')[0].classList.remove('open')
